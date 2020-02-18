fdf26ffe150306247faf36da9049ddd41bcb7ef2
Evgeniy Titarenko <pilotnon@gmail.com>
2020-02-19 01:01:00 +0300

    -- change files

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
-// let a = [3, -12, 0, 4, 5, -8];
-//
-// let b =[];
-// for (let i =0; i < a.length; i++) {
-//     if (a[i]>=0) b.push(a[i]);
-// }
-// console.log(b);
-//
-// let c = a.filter(function(item) {
-//     return item >= 0;
-// });
-//
-// console.log(c);
+let arr = [
+    'Hi',
+    'My name is Jhon',
+    'nice to meet you',
+];
 
-
-
-
-let numbers = [3, -12, 0, 4, 5, -8];
-
-let negNumbers = false;
-
-for(let i = 0; i < numbers.length; i++) {
-    if (numbers[i] < 0) ;
-    negNumbers = true;
-    break;
+const body = document.body;
+for (let i = 0; i < arr.length; i++) {
+   const div = document.createElement('div');
+   div.innerHTML = i + 1 + '. ' + arr[i];
+   body.appendChild(div);
 }
-console.log(negNumbers);
-
-let negNumbers2 = numbers.some(function (item) {
-    return item < 0;
-});
-console.log(negNumbers2);
